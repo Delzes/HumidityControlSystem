c8d39ddce0248054a66d3387f00c1846de105a10
vladt <1@1>
2024-11-16 21:09:06 +0500

    1

--- control_server/controls.py
+++ control_server/controls.py
@@ -5,12 +5,15 @@ MQTT_PORT = 1883
 MQTT_TOPIC_FAN = 'home/fan'
 MQTT_TOPIC_LIGHT = 'home/light'
 MQTT_TOPIC_SPRAYER = 'home/sprayer'
+MQTT_TOPIC_TEMP = 'home/temperature'
 MQTT_TOPIC_FAN_HUMIDITY = 'home/fan/humidity'
 MQTT_TOPIC_LIGHT_LUX = 'home/light/lux_value'
 MQTT_TOPIC_SPRAYER_HUMIDITY = 'home/sprayer/humidity'
+MQTT_TOPIC_TEMP_VALUE = 'home/temperature/temperature_value'
 MQTT_TOPIC_FAN_SETTINGS = 'home/fan/settings'
 MQTT_TOPIC_LIGHT_SETTINGS = 'home/light/settings'
 MQTT_TOPIC_SPRAYER_SETTINGS = 'home/sprayer/settings'
+MQTT_TOPIC_TEMP_SETTINGS = 'home/temperature/settings'
 MQTT_TOPIC_SPRAYER_INCREASE = 'home/sprayer/increase'
 
 humidity_threshold = 60
@@ -19,13 +22,16 @@ light_threshold = 300
 current_light = 0
 sprayer_threshold = 50
 current_sprayer = 100
+temperature_threshold_max = 30
+temperature_threshold_min = 10
+current_temperature = 20
 decreasing = True
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe([(MQTT_TOPIC_FAN_HUMIDITY, 0), (MQTT_TOPIC_FAN_SETTINGS, 0),(MQTT_TOPIC_LIGHT_LUX, 0), (MQTT_TOPIC_LIGHT_SETTINGS, 0), (MQTT_TOPIC_SPRAYER_HUMIDITY, 0), (MQTT_TOPIC_SPRAYER_SETTINGS, 0), (MQTT_TOPIC_SPRAYER_INCREASE, 0)])
+    client.subscribe([(MQTT_TOPIC_FAN_HUMIDITY, 0), (MQTT_TOPIC_FAN_SETTINGS, 0),(MQTT_TOPIC_LIGHT_LUX, 0), (MQTT_TOPIC_LIGHT_SETTINGS, 0), (MQTT_TOPIC_SPRAYER_HUMIDITY, 0), (MQTT_TOPIC_SPRAYER_SETTINGS, 0), (MQTT_TOPIC_SPRAYER_INCREASE, 0), (MQTT_TOPIC_TEMP_VALUE, 0), (MQTT_TOPIC_TEMP_SETTINGS, 0)])
 
 def on_message(client, userdata, msg):
-    global current_humidity, humidity_threshold, current_light, light_threshold, current_sprayer, sprayer_threshold, decreasing
+    global current_humidity, humidity_threshold, current_light, light_threshold, current_sprayer, sprayer_threshold, current_temperature,decreasing
     if msg.topic == MQTT_TOPIC_FAN_HUMIDITY:
         current_humidity = int(msg.payload.decode())
         control_fan()
@@ -45,6 +51,7 @@ def on_message(client, userdata, msg):
         print(f"New sprayer threshold: {sprayer_threshold}")
     elif msg.topic == MQTT_TOPIC_SPRAYER_INCREASE:
         decreasing = msg.payload.decode()
+
 
 def control_fan():
     if current_humidity > humidity_threshold:
